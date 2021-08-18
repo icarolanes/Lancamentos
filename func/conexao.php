7a2217868f9c114cf78e62f6aef4c32a646b00e4
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-08-17 22:53:51 -0300

    Update conexao.php

--- func/conexao.php
+++ func/conexao.php
@@ -1,21 +1,3 @@
 <?php
-try{
-$con = new PDO('mysql:host=localhost;dbname=pedras', 'root', '',
-
-array(
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
-                PDO::ATTR_PERSISTENT => false,
-                PDO::ATTR_EMULATE_PREPARES => false,
-                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-            )
-
-);
-}
-catch (PDOException $e)
-{
-echo 'Error: '. $e->getMessage();
-exit();
-}
-
-$icaro = "icaro";
+include_once('../conexao.php');
 ?>
