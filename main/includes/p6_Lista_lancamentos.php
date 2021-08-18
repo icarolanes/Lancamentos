c23afcc597ba39c3c39caeb60545ac32edadd2fc
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-08-17 21:14:10 -0300

    includes
    Separado algumas linhas em arquivos diferentes sendo chamados via include

---
+++ main/includes/p6_Lista_lancamentos.php
@@ -0,0 +1,108 @@
+        <div class="table-responsive">
+ 
+ <table class="table table-hover table-striped table-sm">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Tipo</th>
+                <th scope="col">Documento</th>
+                <th scope="col">Empresa</th>
+                <th scope="col">NF</th>
+                <th scope="col">Placa</th>
+                <th scope="col">Ori/Dest</th>
+                <th scope="col">Data lançamento</th>
+                <th scope="col">periodo</th>
+                <th scope="col">liquido</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php 
+
+// query dos itens a serem mostrados
+              $query_lanc = "SELECT 
+              lanc.id as tipo,
+              lanc.id as documento,
+              lanc.id as ori,
+              lanc.id as responsavel,
+              lanc.id as nf,
+              lanc.id as motorista,
+              lanc.id as placa,
+              lanc.id as data,
+              lanc.id as dope,
+              lanc.id as periodo,
+              lanc.id as tara,
+              lanc.id as bruto,
+              lanc.id as liquido 
+              from lancamentos lanc 
+              WHERE lanc.atracacao = ".$_GET['n']."
+              order by lanc.id";
+
+//query total para contagem de itens
+              $query_pag = "SELECT * from lancamentos lanc WHERE lanc.atracacao = ".$_GET['n'];
+//executa a query de contagem
+              $resultado_pag = $con ->prepare($query_pag);
+              $resultado_pag ->execute();
+              $contar_lanc = $resultado_pag ->rowCount();
+              if (!isset($_GET['page'])) {
+                $page = 1 ; 
+                $navant = 1;
+              }else{
+                $page = $_GET['page'];
+                switch ($page) {
+                  case '1':
+                  $navant = 1;
+                  $navprox = $page + 1 ;
+                  break;
+                  default:
+                  $navant = $page-1 ;
+                  $navprox = $page + 1 ;
+                  break;
+                }
+              }
+$qpp = 20; //quantidade por paginas
+$inicio = $page-1;
+$inicio = $inicio * $qpp;
+$query_limit = $query_lanc. " LIMIT  $inicio, ".$qpp;
+//ececuta a query da quantidade atual
+$resultado = $con ->prepare($query_limit);
+$resultado ->execute();
+$cnt = 1;
+while ($linha=$resultado->fetch()) {
+              // id`, `tipo`, `documento`, `responsavel`, `nf`, `transp`, `motorista`, `placa`, `data`, `periodo`, `tara`, `bruto`, `liquido`, `porao`
+  ?>
+  <tr>
+    <td>Abrir</td>
+    <td><?php echo $linha['tipo']; ?></td>
+    <td><?php echo $linha['documento']; ?></td>
+    <td><?php echo $linha['responsavel']; ?></td>
+    <td><?php echo $linha['nf']; ?></td>
+    <td title="<?php echo $linha['motorista']; ?>"><?php echo $linha['placa']; ?></td>
+    <td><?php echo $linha['ori']; ?></td>
+    <td><?php echo $linha['data']; ?></td>
+    <td title="<?php echo $linha['dope']; ?>"><?php echo $linha['periodo']; ?></td>
+    <td><?php echo $linha['liquido']; ?></td>
+  </tr>
+  <?php
+}
+?>
+</tbody>
+</table>
+
+<nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item"><a class="page-link" href="index.php?<?php echo "p=".$_GET['p']."&n=".$_GET['n']."&page=". $navant ?>">Anterior
+    </a></li>
+    <?php  
+//paginação
+    $pages = $contar_lanc / $qpp;
+    $nav = 1;
+    for ($i=0; $i < $pages ; $i++) { 
+      ?>
+      <li class="page-item"><a class="page-link" href="index.php?<?php echo "p=".$_GET['p']."&n=".$_GET['n']."&page=". $nav ?>"><?php echo $nav++ ?></a></li>
+      <?php  
+    }
+    ?>
+    <li class="page-item"><a class="page-link" href="index.php?<?php echo "p=".$_GET['p']."&n=".$_GET['n']."&page=". $navprox ?>">Proxima</a></li>
+  </ul>
+</nav>
+</div>
---
+++ main/includes/p6_atracacao_selecao.php
@@ -0,0 +1,46 @@
+<?php // lista de atracações ativas, listagem por card
+
+$queryATR = "SELECT 
+at.id as id, 
+at.Datracacao as atraca,
+at.ope1 as inicio, 
+at.ope2 as termino, 
+at.Ddesatracacao as desatraca,
+sum(lanc.liquido) as ttoe,
+count(lanc.id) as veic,
+
+nav.nNome as navio 
+FROM atracacao at 
+join navios nav on at.navio = nav.id 
+left join lancamentos lanc on lanc.atracacao = at.id 
+
+WHERE at.situacao = 1 group by at.id";
+$prepara = $con->prepare($queryATR);
+$prepara->execute();
+while ($atracacao=$prepara->fetch()) {
+  ?>
+
+
+  <div class="row row-cols-1 row-cols-md-4 g-4">
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">MV <?php echo $atracacao['navio'];?></h5>
+          <p class="card-text">
+            A: <?php  echo date('d/m/y H:i', strtotime($atracacao['atraca'])); ?> <br>
+            I: <?php  echo date('d/m/y H:i', strtotime($atracacao['inicio'])); ?><br>
+            T: <?php  echo date('d/m/y H:i', strtotime($atracacao['termino'])); ?><br>
+            D: <?php  echo date('d/m/y H:i', strtotime($atracacao['desatraca'])); ?> <br>
+          </p>
+          <button onclick="window.location.href='index.php?p=6&n=<?php echo $atracacao['id']?>'" class="btn btn-primary"> abrir</button>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted"><?php echo "TTo: ". ($atracacao['ttoe']/1000)." | Tv: ".$atracacao['veic']." "?> </small>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <?php
+}
