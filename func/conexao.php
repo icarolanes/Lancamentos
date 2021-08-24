440d354c8fce4140c534a9d88f55ff61925bf54d
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-08-23 21:42:51 -0300

    Update conexao.php

--- func/conexao.php
+++ func/conexao.php
@@ -1,3 +1,31 @@
 <?php
 include_once('../conexao.php');
+
+
+/*
+
+o metodo de conexão que usei foi o PDO.
+
+try{
+$con = new PDO('mysql:host=localhost;dbname=bd_name', 'user', 'pass', // conexao google hosts
+
+array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
+                PDO::ATTR_PERSISTENT => false,
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+            )
+
+);
+}
+catch (PDOException $e)
+{
+echo 'Error: '. $e->getMessage();
+exit();
+}
+
+
+
+
+*/
 ?>
