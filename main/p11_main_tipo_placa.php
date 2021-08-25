5be48e0137077e272f0fa4498dc7dbeb894ddbe3
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-08-25 11:09:51 -0300

    Botões de ação
    Incluso Botões de editar e excluir

--- main/p11_main_tipo_placa.php
+++ main/p11_main_tipo_placa.php
@@ -13,8 +13,9 @@
         <table class="table table-hover table-striped table-sm">
           <thead>
             <tr>
-              <th scope="col">#</th>
+              <th scope="col" colspan="2">#</th>
               <th scope="col">Tipo</th>
+              <th scope="col">Descrição</th>
             </tr>
           </thead>
           <tbody>
@@ -26,52 +27,50 @@
             while ($linha=$resultado->fetch()) {
               ?>
               <tr>
-                <td>Editar - Excluir</td>
+                <td><a href="#" ><span class="text-warning" data-feather="edit"></span></a></td>
+                <td><a href="#" ><span class="text-danger" data-feather="trash"></span></a></td>
                 <td><?php echo $linha['tipo']; ?></td>
+                <td><?php echo $linha['descr']; ?></td>
               </tr>
               <?php
             }
             ?>
-            
+
           </tbody>
         </table>
       </div>
 
 
-<!-- Modal -->
-<div class="modal fade" id="novo_tipo_placa" tabindex="-1" aria-labelledby="label_tipo_placa" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="label_tipo_placa">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        
+      <!-- Modal -->
+      <div class="modal fade" id="novo_tipo_placa" tabindex="-1" aria-labelledby="label_tipo_placa" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="label_tipo_placa">Modal title</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
 
-         <form method="post" action="func/p11_cad_tipo_placa.php" id="cad_tipo_placa">
-          <div class="mb-3">
-            <label for="recipient-name" class="col-form-label">Tipo:</label>
-            <input type="text" class="form-control" id="tipo_placa">
+
+             <form method="post" action="func/p11_cad_tipo_placa.php" id="cad_tipo_placa">
+              <div class="mb-3">
+                <label for="recipient-name" class="col-form-label">Tipo:</label>
+                <input type="text" class="form-control" id="tipo_placa" required>
+              </div>
+              <div class="mb-3">
+                <label for="message-text" class="col-form-label">Descrição:</label>
+                <input type="text" class="form-control" id="descricao_placa" required>
+              </div>
+            </form>
+
+
+
           </div>
-          <div class="mb-3">
-            <label for="message-text" class="col-form-label">Descrição:</label>
-            <input type="text" class="form-control" id="descricao_placa">
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary" form="cad_tipo_placa">Salvar</button>
           </div>
-        </form>
-
-
-
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary" form="cad_tipo_placa">Salvar</button>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-
-
-
-
-    </main>
+  </main>
