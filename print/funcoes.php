5edacecae242658841af0e8fa7b3664c70c8f966
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-09-01 20:47:17 -0300

    funcao
    nova função. Meta por dia.

--- print/funcoes.php
+++ print/funcoes.php
@@ -1,6 +1,6 @@
 <?php
+//formato de data
 function datahora($data,$tipo){
-
   switch ($tipo) {
     case '1':
     $data = date('d/m/y H:i', strtotime($data));
@@ -8,20 +8,28 @@ function datahora($data,$tipo){
     
     case '2':
     
-
     break;
-
     default:
     // code...
     break;
   }
   return($data);
 };
+//formatação de numero
 function numero($numero){
   $numero = number_format($numero,0,',','.');
   return($numero);
 }
 
+//verificar meta
+function meta($meta,$total){
 
+  if ($meta <= $total) {
+    $retorno = "Meta alcançada";
+  }else{
+    $retorno = "Meta não alcançada";
+  }
 
+  return($retorno);
+}
 ?>
