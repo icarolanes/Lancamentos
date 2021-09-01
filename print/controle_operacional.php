52a022a346aaed14a1a6bd4a618638b608f8288f
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-09-01 15:15:28 -0300

    conexoes
    criando conexoes da tabela

--- print/controle_operacional.php
+++ print/controle_operacional.php
@@ -1,7 +1,7 @@
   <html lang="pt-br">
   <meta charset="utf-8">
   <?php
-  include('../func/conexao.php');
+  include('../../conexao.php');
   include('funcoes.php');
   function cop($con,$id){
 
@@ -23,27 +23,38 @@
       $pors2 .= " (select sum(total) from plano_ope_porao where porao = ".$po2.") as porao".$po2.",";
       $po2++;
     }
-    $atracacao_dados = "SELECT navio as navio, Natraca as numero, Datracacao as atracacao, Ddesatracacao as desatracacao, ope1 as inicio_ope, ope2 as termino_ope,timediff(ope2,ope1) as Toperacao , timediff(Ddesatracacao, Datracacao) as Tatracacao from atracacao;";
+    $atracacao_dados = "SELECT navio as navio, Natraca as numero, Datracacao as atracacao, Ddesatracacao as desatracacao, ope1 as inicio_ope, ope2 as termino_ope,timediff(ope2,ope1) as Toperacao , timediff(Ddesatracacao, Datracacao) as Tatracacao from atracacao where id = $id;";
 
 
     
     $prep_at = $con->prepare($atracacao_dados);
     $prep_at->execute();
     $dados_at = $prep_at->fetch();
-   
-    $c =0;
+
+    $c = 0;
     $acum = 0;
+
+
+
+
+//consulta datas distintas de operação
+    $dias_ope = "SELECT DISTINCT(data_ref) FROM periodos_operacao where atracacao = $id";
+    $prep_d_ope = $con->prepare($dias_ope);
+    $prep_d_ope->execute();
+
+
     include_once('print_teste_cop.php');
-}//end function
-if (isset($_GET['n'])) {
-  cop($con,$_GET['n']);
-}else{
-}
-?>
-<form action="#">
-	
-  <label>ID da atracação</label>
-  <input type="number" name="n">
-  <button type="submit">ok</button>
-</form>
-</html>
+  }
+//end function
+  if (isset($_GET['n'])) {
+    cop($con,$_GET['n']);
+  }else{
+  }
+  ?>
+  <form action="#">
+
+    <label>ID da atracação</label>
+    <input type="number" name="n">
+    <button type="submit">ok</button>
+  </form>
+  </html>
