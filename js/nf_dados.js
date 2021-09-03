12308cdf422191c415a7524223644b28da20b462
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-09-02 23:36:08 -0300

    Dados
    Separação de dados em chave de NF completa

--- js/nf_dados.js
+++ js/nf_dados.js
@@ -5,12 +5,24 @@
 		var nNF		= (document.getElementById('nNF').value);
 
 		var cUF 	= chave.substring(0,2);
-		var tpEmis 	= chave.substring(2,3);
-		var cnpj 	= chave.substring(3,17);
+		var AME 	= chave.substring(2,6);
+		var cnpj 	= chave.substring(6,20);
+		var mod		= chave.substring(20,22);
+		var serie	= chave.substring(22,25);
+		var nNF		= chave.substring(25,34);
+		var FEmi	= chave.substring(34,35);
+		var CN		= chave.substring(35,43);
+		var DV		= chave.substring(43,44);
 
-		document.getElementById('cUF').value       = cUF;
-		document.getElementById('tpEmis').value    = tpEmis;
-		document.getElementById('cnpj').value      = cnpj;
-		document.getElementById('nNF').value       = chave;
+		cnpj_f = cnpj.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2}).*/,"$1.$2.$3/$4-$5");
 
+		document.getElementById('cUF').value       	= cUF;
+		document.getElementById('AME').value 		= AME;
+		document.getElementById('cnpj').value      	= cnpj_f;
+		document.getElementById('mod').value       	= mod;
+		document.getElementById('serie').value      = serie;
+		document.getElementById('nNF').value       	= nNF;
+		document.getElementById('FEmi').value       = FEmi;
+		document.getElementById('CN').value       	= CN;
+		document.getElementById('DV').value       	= DV;
 	}
--- main/p15_main_form_cadastro_nf.php
+++ main/p15_main_form_cadastro_nf.php
@@ -1,29 +1,68 @@
+    <script type="text/javascript" src="js/nf_dados.js"></script>
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Cadastro De NF</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
-         
+
         </div>
       </div>
-<div class=" col-sm-12 col-md-12">
-   
-  <form class="row g-2" method="post" action="func/lanc_enviar.php">
+      <div class=" col-sm-12 col-md-12">
+
+        <form oninput="separa_dados();" class="row g-2" method="post" action="func/lanc_enviar.php">
 
 
-<div class="col-md-12 mb-3">
-  <label for="chaveNota" class="form-label">Chave da NF</label>
-  <input type="text" class="form-control" id="chaveNota">
-</div>
+          <div class="col-md-12 mb-3">
+            <label for="chaveNota" class="form-label">Chave da NF</label>
+            <input type="text" class="form-control" id="chaveNota" value="31210524042913000139550000000164641100164649" maxlength="44" autofocus >
+          </div>
 
-<div class="col-md-2 mb-3">
-  <label for="chaveNota" class="form-label">Chave da NF</label>
-  <input type="text" class="form-control" id="chaveNota">
-</div>
-<div class="col-md-2 mb-3">
-  <label for="chaveNota" class="form-label">Chave da NF</label>
-  <input type="text" class="form-control" id="chaveNota">
-</div>
+          <div class="col-md-1 mb-3">
+            <label for="cUF" class="form-label">UF</label>
+            <input type="text" class="form-control" id="cUF" readonly>
+          </div>
 
+          <div class="col-md-1 mb-3">
+            <label for="AME" class="form-label">MES E ANO</label>
+            <input type="text" class="form-control" id="AME" readonly>
+          </div>
 
-</form>
-</div></main>
+          <div class="col-md-2 mb-3">
+            <label for="cnpj" class="form-label">CNPJ</label>
+            <input type="text" class="form-control" id="cnpj" readonly>
+          </div>
+          
+          <div class="col-md-2 mb-3">
+            <label for="mod" class="form-label">Modelo</label>
+            <input type="text" class="form-control" id="mod" readonly>
+          </div>
+
+          <div class="col-md-2 mb-3">
+            <label for="serie" class="form-label">Serie</label>
+            <input type="text" class="form-control" id="serie" readonly>
+          </div>
+
+          <div class="col-md-2 mb-3">
+            <label for="nNF" class="form-label">Numero Nota</label>
+            <input type="text" class="form-control" id="nNF" readonly>
+          </div>
+
+          <div class="col-md-2 mb-3">
+            <label for="FEmi" class="form-label">F emissao</label>
+            <input type="text" class="form-control" id="FEmi" readonly>
+          </div>
+          
+          <div class="col-md-2 mb-3">
+            <label for="CN" class="form-label">C Num</label>
+            <input type="text" class="form-control" id="CN" readonly>
+          </div>
+          
+
+          <div class="col-md-2 mb-3">
+            <label for="DV" class="form-label">DV</label>
+            <input type="text" class="form-control" id="DV" readonly>
+          </div>
+          
+
+        </form>
+      </div>
+    </main>
