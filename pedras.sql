0a3043e66bfe956f1a586e9d1bcbf5bf6fe4396d
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-09-03 09:04:48 -0300

    banco
    Novo arquivo de banco

--- pedras.sql
+++ pedras.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 17-Ago-2021 às 19:31
+-- Tempo de geração: 31-Ago-2021 às 09:54
 -- Versão do servidor: 10.4.20-MariaDB
 -- versão do PHP: 8.0.8
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `pedras`
 --
-CREATE DATABASE IF NOT EXISTS `pedras` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `pedras`;
 
 -- --------------------------------------------------------
 
