b8dfeeefc00c06c4e0018dcc885a6bb6068dfb90
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-09-03 16:40:00 -0300

    DV da NF
    Calculo de DV de NF

--- js/nf_dados.js
+++ js/nf_dados.js
@@ -1,9 +1,10 @@
 	function separa_dados(){
 		//Chave da nota
 		var chave 	= (document.getElementById('chaveNota').value);
+		var chave_ver 	= (document.getElementById('chaveNota').value);
 		var cnpj 	= (document.getElementById('cnpj').value);
 		var nNF		= (document.getElementById('nNF').value);
-
+		//separação de dados
 		var cUF 	= chave.substring(0,2);
 		var AME 	= chave.substring(2,6);
 		var cnpj 	= chave.substring(6,20);
@@ -13,9 +14,9 @@
 		var FEmi	= chave.substring(34,35);
 		var CN		= chave.substring(35,43);
 		var DV		= chave.substring(43,44);
-
+		//formatação de cnpj
 		cnpj_f = cnpj.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2}).*/,"$1.$2.$3/$4-$5");
-
+		//retorno para campos
 		document.getElementById('cUF').value       	= cUF;
 		document.getElementById('AME').value 		= AME;
 		document.getElementById('cnpj').value      	= cnpj_f;
@@ -25,4 +26,32 @@
 		document.getElementById('FEmi').value       = FEmi;
 		document.getElementById('CN').value       	= CN;
 		document.getElementById('DV').value       	= DV;
-	}
+		//DV da NF
+		if(chave_ver.length == 44){
+			var chave43 = chave_ver;
+			multiplicadores = ['2','3','4','5','6','7','8','9'];
+			i = 42;
+			soma_ponderada = 0;
+			while (i >= 0) {
+				for (m=0; m< multiplicadores.length && i>=0; m++) {
+					soma_ponderada += chave43[i] * multiplicadores[m];
+					i--;
+				}
+			}
+			resto = soma_ponderada % 11;
+			if (resto == '0' || resto == '1') {
+				dv = 0;
+			}else{
+				dv = 11 - resto;
+			}
+			if (dv == DV) {
+				document.getElementById('msg').innerHTML = "Chave de acesso Válida";
+			}else{
+				document.getElementById('msg').innerHTML = "Chave de acesso invalida.";
+			}
+		}else{
+			document.getElementById('msg').innerHTML = 'Restam ';
+			document.getElementById('msg').innerHTML += 44 - chave_ver.length ;
+			document.getElementById('msg').innerHTML += ' digitos para chave de acesso' ;
+		}//fim DV da NF
+	} //fim separa_dados
