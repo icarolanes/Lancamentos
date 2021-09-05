77a452f3d911b4db87870db629f40d7d658461e5
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-09-05 15:36:07 -0300

    erro
    há um erro......

--- print/resumo_operacional.php
+++ print/resumo_operacional.php
@@ -56,17 +56,142 @@
       left join lancamentos lanc on ope.id = lanc.operacao 
       GROUP by atr.id,pope.id";
 
-      $select;
-
-
-
       $prepare = $con->prepare($select);
       $prepare->execute();
       $contar_periodo = $prepare ->rowCount();
       $c =0;
       $acum = 0;
 
-      include_once('print_teste_rop.php');
+?>
+
+
+
+<table border="1px" style="width: 100%;">
+  <thead>
+    <tr>
+      <th colspan="5">sadasd</th>
+    </tr>
+  </thead>
+</table>
+
+  <table border="1px" >
+    <thead>
+      <tr>
+        <th colspan="3">Resumo Operacional - ROP<br><small> Summary Control</small></th>
+        <th colspan="<?php echo $poroes['poroes']; ?>" rowspan="4">Operador Portuário <br> <small>Port Operator</small><br> LOGO</th>
+        <th colspan="1">Navegação</th>
+        <th colspan="3">longo ou cabotagem</th>
+      </tr>
+      <tr>
+        <th colspan="3">MV <?php echo $dados_at['navio'] ?></th>
+        <th rowspan="1">operação</th>
+        <th colspan="3" >Embarque</th>
+      </tr>
+      <tr>
+        <th>Berço</th>
+        <th>Inicio</th>
+        <th colspan="1"><?php echo datahora($dados_at['ope1'],1); ?></th>
+        <th rowspan="2">Porões</th>
+        <th colspan="3" rowspan="2"><?php echo $dados_at['poroes'];?></th>
+      </tr>
+      <tr>
+        <th><?php echo $dados_at['berco']; ?></th>
+        <th>Termino</th>
+        <th colspan="1"><?php echo datahora($dados_at['ope2'],1); ?></th>
+      </tr>
+      <tr>
+        <th rowspan="2">Seq</th>
+        <th rowspan="2">Data</th>
+        <th rowspan="2">Periodo</th>
+        <th colspan="<?php echo $poroes['poroes']; ?>">Porões</th>
+        <th rowspan="2" colspan="2">Paralizações</th>
+        <th rowspan="2">Total</th>
+        <th rowspan="2">Acumulado</th>
+      </tr>
+      <tr>
+        <?php for ($pori=1; $pori <= $poroes['poroes'] ; $pori++) { 
+         echo "<th width='5%'>#".$pori."</th>";
+         $acum_por[$pori] = 0;
+       }
+       ?>
+     </tr>
+   </thead>
+   <tbody>
+     <?php
+     for ($i=0; $i < $contar_periodo; $i++) { 
+      $linha = $prepare->fetch();
+      ?>
+      <tr>
+       <td><?php echo ++$c;?></td>
+       <td><?php echo date('d/m/y', strtotime($linha['dope']))?></td>
+       <td><?php echo $linha['periodo']?></td>
+       <?php  for ($pori=1; $pori <= $poroes['poroes'] ; $pori++) {  ?>
+        <td><?php echo numero($linha['porao'.$pori]); ?></td>
+        <?php  $acum_por[$pori] = $acum_por[$pori] +  $linha['porao'.$pori];   }   ?>
+        <td><?php echo $linha['paralizacoes']  ; ?></td>
+        <td><?php echo $linha['tempo']  ; ?></td>
+        <td><?php echo numero($linha['total'])?></td>
+        <td><?php echo numero($acum = $acum+$linha['total'])?></td>
+      </tr>
+      <?php
+    }
+    ?>
+  </tbody>
+  <thead>
+    <tr>
+      <td colspan="3">Porões</td>
+      <?php for ($pori=1; $pori <= $poroes['poroes'] ; $pori++) { 
+        echo "<td>#".$pori."</td>";
+      }
+      ?>
+      <td colspan="2"></td>
+      <td colspan="2" >Total</td>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td colspan="3">Total Manifestado</td>
+      <?php for ($pori=1; $pori <= $poroes['poroes'] ; $pori++) {    ?>
+       <td><?php echo numero($dados_at['porao'.$pori]); ?> </td> <?php }  ?>
+       <td colspan="2"></td>
+       <td colspan="2"><?php echo numero($dados_at['total']);?></td>
+     </tr>
+     <tr>
+      <td colspan="3">Total descarregado</td>
+      <?php  for ($pori=1; $pori <= $poroes['poroes'] ; $pori++) { 
+       echo "<td>".numero($acum_por[$pori])."</td>";
+     }
+     ?>
+     <td colspan="2"></td>
+     <td colspan="2"><?php echo numero($acum)?></td>
+   </tr>
+   <tr>
+    <td colspan="3">Saldo</td>
+    <?php for ($pori=1; $pori <= $poroes['poroes'] ; $pori++) { 
+     $saldo_por[$pori] = $dados_at['porao'.$pori] - $acum_por[$pori];
+     echo "<td>".numero($saldo_por[$pori])."</td>";
+   }
+   ?>
+   <td colspan="2"></td>
+   <td colspan="2"><?php echo numero($saldo_total = $dados_at['total'] - $acum)?></td>
+ </tr>
+</tbody>
+</table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+      
 }//end function
 if (isset($_GET['n'])) {
   rop($con,$_GET['n']);
