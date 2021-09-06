0dde5061d7cd0e36915b893848d8dbec834964ad
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-09-06 10:32:12 -0300

    impressão
    Landscape
    @page{
    size: landscape;
    }

--- print/resumo_operacional.php
+++ print/resumo_operacional.php
@@ -1,83 +1,96 @@
     <html lang="pt-br">
     <link rel="stylesheet" type="text/css" href="css/impressoes.css">
-    <meta charset="utf-8">
-    <?php
-    include('../../conexao.php');
-    include('../funcoes/tratamentos.php');
-    function obs($manifestado,$descarregado,$quebra){
-
-      if ($manifestado > $descarregado) {
-        $mensagem['br']   = "Foi descarregado  ".numero($quebra)." a menos do total manifestado";
-        $mensagem['eng']  = numero($quebra)." Less was discharged from manifested total";
-      }else{
-        $mensagem['br']  = "Foi descarregado  ".numero($quebra)." a mais do total manifestado";
-        $mensagem['eng'] = numero($quebra)." More was discharged from manifested total";
+    <style type="text/css">
+    @media print{
+      @page {
+        size: landscape;  
       }
-      return ($mensagem);
     }
-    function rop($con,$id){
+  </style>
+  <script type="text/javascript">
+    window.onbeforeprint = (event) => {
+  console.log('Before print');
+};
+  </script>
+  <meta charset="utf-8">
+  <?php
+  include('../../conexao.php');
+  include('../funcoes/tratamentos.php');
+  function obs($manifestado,$descarregado,$quebra){
+
+    if ($manifestado > $descarregado) {
+      $mensagem['br']   = "Foi descarregado  ".numero($quebra)." a menos do total manifestado";
+      $mensagem['eng']  = numero($quebra)." Less was discharged from manifested total";
+    }else{
+      $mensagem['br']  = "Foi descarregado  ".numero($quebra)." a mais do total manifestado";
+      $mensagem['eng'] = numero($quebra)." More was discharged from manifested total";
+    }
+    return ($mensagem);
+  }
+  function rop($con,$id){
 //Select dados de atracação
-      $qporoes = "select poroes from navios where id =".$id." limit 1";
-      $prepara = $con->prepare($qporoes);
-      $prepara->execute();
-      $poroes = $prepara->fetch();
-      $poroes['poroes'];
-      $po = 1;
-      $pors = "";
-      for ($linhas=0; $linhas < $poroes['poroes'] ; $linhas++) { 
-        $pors .= " (SELECT sum(lanc2.liquido) from lancamentos lanc2 join operacao ope2 on ope2.id = lanc2.operacao where ope2.periodo = pope.id and ope2.armazem = ".$po.") as porao".$po.",";
-        $po++;
-      }
-      $po2 = 1;
-      $pors2 = "";
-      for ($linhas2=0; $linhas2 < $poroes['poroes'] ; $linhas2++) { 
-        $pors2 .= " (select sum(plan.total) from plano_ope_porao plan where plan.porao = ".$po2.") as porao".$po2.",";
-        $po2++;    
-      }
-      $atracacao_dados = "SELECT 
-      n.Nnome as navio,
-      atr.ope1,
-      atr.ope2, 
-      b.numero as berco, 
-      ( select GROUP_CONCAT(arm.descr SEPARATOR ' ') from plano_ope_porao pl join armazem arm on pl.porao = arm.numero where atracacao = atr.id ) as poroes,
-      ".$pors2." 
-      (select sum(total) from plano_ope_porao where atracacao = atr.id) as total
-      FROM atracacao atr join navios n on atr.navio = n.id join berco b on atr.berco = b.id join lancamentos lanc on lanc.atracacao = atr.id join plano_ope_porao plan on plan.atracacao = atr.id where atr.id = ".$id." group by atr.id";
+    $qporoes = "select poroes from navios where id =".$id." limit 1";
+    $prepara = $con->prepare($qporoes);
+    $prepara->execute();
+    $poroes = $prepara->fetch();
+    $poroes['poroes'];
+    $po = 1;
+    $pors = "";
+    for ($linhas=0; $linhas < $poroes['poroes'] ; $linhas++) { 
+      $pors .= " (SELECT sum(lanc2.liquido) from lancamentos lanc2 join operacao ope2 on ope2.id = lanc2.operacao where ope2.periodo = pope.id and ope2.armazem = ".$po.") as porao".$po.",";
+      $po++;
+    }
+    $po2 = 1;
+    $pors2 = "";
+    for ($linhas2=0; $linhas2 < $poroes['poroes'] ; $linhas2++) { 
+      $pors2 .= " (select sum(plan.total) from plano_ope_porao plan where plan.porao = ".$po2.") as porao".$po2.",";
+      $po2++;    
+    }
+    $atracacao_dados = "SELECT 
+    n.Nnome as navio,
+    atr.ope1,
+    atr.ope2, 
+    b.numero as berco, 
+    ( select GROUP_CONCAT(arm.descr SEPARATOR ' ') from plano_ope_porao pl join armazem arm on pl.porao = arm.numero where atracacao = atr.id ) as poroes,
+    ".$pors2." 
+    (select sum(total) from plano_ope_porao where atracacao = atr.id) as total
+    FROM atracacao atr join navios n on atr.navio = n.id join berco b on atr.berco = b.id join lancamentos lanc on lanc.atracacao = atr.id join plano_ope_porao plan on plan.atracacao = atr.id where atr.id = ".$id." group by atr.id";
 
-      $prep_at = $con->prepare($atracacao_dados);
-      $prep_at->execute();
-      $dados_at = $prep_at->fetch();
-
-
-      $select = "
-      SELECT 
-      atr.id,
-      pope.data_ref as dope,
-      pope.periodo as periodo,";
-      $select.= $pors ;
-      $select .= "sum(lanc.liquido) as total,
-      (select GROUP_CONCAT(lpar1.pt_br separator '<br>') from paralizacao_ope para join lista_paralizacoes lpar1 on lpar1.id = para.paralizacao where para.periodo = pope.id ) as paralizacoes,
-      (select GROUP_CONCAT(timediff(para.par_termino, para.par_inicio)separator '<br>') from paralizacao_ope para where para.periodo = pope.id ) as tempo
-      from atracacao atr 
-      join periodos_operacao pope on pope.atracacao = atr.id
-      LEFT join operacao ope on ope.periodo = pope.id
-      left join lancamentos lanc on ope.id = lanc.operacao 
-      where atr.id = ".$id."
-      GROUP by atr.id,pope.id";
-
-      $prepare = $con->prepare($select);
-      $prepare->execute();
-      $contar_periodo = $prepare ->rowCount();
-      $c =0;
-      $acum = 0;
+    $prep_at = $con->prepare($atracacao_dados);
+    $prep_at->execute();
+    $dados_at = $prep_at->fetch();
 
 
+    $select = "
+    SELECT 
+    atr.id,
+    pope.data_ref as dope,
+    pope.periodo as periodo,";
+    $select.= $pors ;
+    $select .= "sum(lanc.liquido) as total,
+    (select GROUP_CONCAT(lpar1.pt_br separator '<br>') from paralizacao_ope para join lista_paralizacoes lpar1 on lpar1.id = para.paralizacao where para.periodo = pope.id ) as paralizacoes,
+    (select GROUP_CONCAT(timediff(para.par_termino, para.par_inicio)separator '<br>') from paralizacao_ope para where para.periodo = pope.id ) as tempo
+    from atracacao atr 
+    join periodos_operacao pope on pope.atracacao = atr.id
+    LEFT join operacao ope on ope.periodo = pope.id
+    left join lancamentos lanc on ope.id = lanc.operacao 
+    where atr.id = ".$id."
+    GROUP by atr.id,pope.id";
 
-      $select_documentos = "SELECT *, (select sum(liquido) from lancamentos where documento = doc.id) as descarregado FROM documentos doc where atracacao = ".$id;
-      $prep_doc = $con->prepare($select_documentos);
-      $prep_doc->execute();
+    $prepare = $con->prepare($select);
+    $prepare->execute();
+    $contar_periodo = $prepare ->rowCount();
+    $c =0;
+    $acum = 0;
 
-      ?>
+
+
+    $select_documentos = "SELECT *, (select sum(liquido) from lancamentos where documento = doc.id) as descarregado FROM documentos doc where atracacao = ".$id;
+    $prep_doc = $con->prepare($select_documentos);
+    $prep_doc->execute();
+
+    ?>
+    <body>
       <table class="rop">
         <thead>
           <tr>
@@ -200,7 +213,7 @@
       ?>
       <tr>
         <td colspan="3"><?php echo $docs['identificacao']?></td>
-        <td colspan="<?php echo $poroes['poroes'];?>">BL do produto</td>
+        <td colspan="<?php echo $poroes['poroes'];?>"><?php echo $docs['conhecimento']?></td>
         <td colspan="2"><?php $soma_manif = $soma_manif+$docs['manifestado']; echo numero($docs['manifestado'])?></td>
         <td colspan="1"><?php $soma_desc = $soma_desc+$docs['descarregado']; echo numero($docs['descarregado'])?></td>
         <td colspan="1"><?php $quebra = $docs['manifestado']-$docs['descarregado']; $soma_quebra = $soma_quebra+($quebra); echo numero($quebra)?></td>
@@ -217,7 +230,8 @@
     </tr>
     <tr class="obs">
       <td class="obs" colspan="5"><h4>Observações:</h4>
-        <p>
+        <p></p>
+        <p class="auto">
           <?php 
           $mensagem = obs($soma_manif,$soma_desc,$soma_quebra);
           echo $mensagem['br'];
@@ -247,4 +261,5 @@ if (isset($_GET['n'])) {
   <input type="number" name="n">
   <button type="submit">ok</button>
 </form>
+</body>
 </html>
