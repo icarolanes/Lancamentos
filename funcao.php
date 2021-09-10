50373a2195843e0b2bfc81e155c1486e3e38b0da
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-09-09 22:25:43 -0300

    comentario
    Limpeza de espaço em branco e comentario

--- funcao.php
+++ funcao.php
@@ -1,30 +1,20 @@
 <?php
-
+/* Arquivo para gerar um JSON de informações de meu banco*/
 include_once("banco/conexao.php");
 function retorna($motorista,$con,$icaro){
-
 	$query_mot = "SELECT * from pessoa where cpf = ".$_GET['cpf'];
 	$resultado = $con->prepare($query_mot);
 	$resultado->execute();
 	$contar_lanc = $resultado ->rowCount();
 	
 	$linha=$resultado->fetch();
-
 	if ($contar_lanc>0) {
-
 		$valores['motorista'] = $linha['nome']." ".$linha['sobrenome'];
-
 	}else{
-
 		$valores['motorista'] = "";
-
-
 	}
-
-
 	return json_encode($valores);
 }
-
 if(isset($_GET['cpf'])){
 	echo retorna($_GET['cpf'],$con,$icaro);
 }
