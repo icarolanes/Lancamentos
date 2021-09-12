9c33dc058448d401a8d50402858e584464a7fde5
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-09-12 12:01:11 -0300

    select
    Seleção de cnpj junto do cep cadastrado relacionado

--- banco/p3_cons_cnpj_banco.php
+++ banco/p3_cons_cnpj_banco.php
@@ -1,17 +1,22 @@
 <?php
 include_once('../../conexao.php');
 function retorna($cnpj,$con){
-	$query_em = ("SELECT * from empresas where xCNPJ = ".$cnpj);
+	$query_em = ("SELECT emp.id as id, emp.xCNPJ, emp.xNome, emp.fantasia, cep.cep, cep.estado,cep.cidade,cep.bairro,cep.rua FROM `empresas` emp join `lista_cep` cep on cep.id = emp.cep where xCNPJ = ".$cnpj);
 	$prep = $con->prepare($query_em);
 	$prep->execute();
 	$contar = $prep->rowCount();
 	$linha = $prep ->fetch();
 	if ($contar > 0) {
-		$retorno['id'] = $linha['id'];
-		$retorno['cnpj'] = $linha['xCNPJ'];
-		$retorno['razao'] = $linha['xNome'];
-		$retorno['fantasia'] = $linha['fantasia'];
-		$retorno['situacao'] = true;
+		$retorno['id']        = $linha['id'];
+		$retorno['cnpj']      = $linha['xCNPJ'];
+		$retorno['razao']     = $linha['xNome'];
+		$retorno['fantasia']  = $linha['fantasia'];
+		$retorno['cep']       = $linha['cep'];
+		$retorno['estado']    = $linha['estado'];
+		$retorno['cidade']    = $linha['cidade'];
+		$retorno['bairro']    = $linha['bairro'];
+		$retorno['rua']       = $linha['rua'];
+		$retorno['situacao']  = true;
 	}else{
 		$retorno['situacao'] = false;
 	}
