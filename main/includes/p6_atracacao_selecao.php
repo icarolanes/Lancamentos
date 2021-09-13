ba28c4f2d75e9d319dfa0b6315c12a99c6929e00
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-09-13 13:42:15 -0300

    Troca de regra
    Mudança de arquivos para novas paginas

--- main/includes/p6_atracacao_selecao.php
+++ main/includes/p6_atracacao_selecao.php
@@ -21,26 +21,30 @@ while ($atracacao=$prepara->fetch()) {
   ?>
 
 
-  <div class="row row-cols-1 row-cols-md-4 g-4">
+<div class="row row-cols-1 row-cols-md-4 g-4">
     <div class="col">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">MV <?php echo $atracacao['navio'];?></h5>
-          <p class="card-text">
-            A: <?php  echo date('d/m/y H:i', strtotime($atracacao['atraca'])); ?> <br>
-            I: <?php  echo date('d/m/y H:i', strtotime($atracacao['inicio'])); ?><br>
-            T: <?php  echo date('d/m/y H:i', strtotime($atracacao['termino'])); ?><br>
-            D: <?php  echo date('d/m/y H:i', strtotime($atracacao['desatraca'])); ?> <br>
-          </p>
-          <button onclick="window.location.href='index.php?p=6&n=<?php echo $atracacao['id']?>'" class="btn btn-primary"> abrir</button>
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">MV <?php echo $atracacao['navio'];?></h5>
+                <p class="card-text">
+                    A: <?php  echo date('d/m/y H:i', strtotime($atracacao['atraca'])); ?> <br>
+                    I: <?php  echo date('d/m/y H:i', strtotime($atracacao['inicio'])); ?><br>
+                    T: <?php  echo date('d/m/y H:i', strtotime($atracacao['termino'])); ?><br>
+                    D: <?php  echo date('d/m/y H:i', strtotime($atracacao['desatraca'])); ?> <br>
+                </p>
+                <button onclick="window.location.href='index.php?p=6&n=<?php echo $atracacao['id']?>'"
+                    class="btn btn-primary"> abrir</button>
+            </div>
+            <div class="card-footer">
+                <small
+                    class="text-muted"><?php echo "TTo: ". ($atracacao['ttoe']/1000)." | Tv: ".$atracacao['veic']." "?>
+                </small>
+            </div>
         </div>
-        <div class="card-footer">
-          <small class="text-muted"><?php echo "TTo: ". ($atracacao['ttoe']/1000)." | Tv: ".$atracacao['veic']." "?> </small>
-        </div>
-      </div>
     </div>
-  </div>
+</div>
 
 
-  <?php
+<?php
 }
+?>
