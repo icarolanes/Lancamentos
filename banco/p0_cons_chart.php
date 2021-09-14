6602c595d0822b5789aa0b28a75d95992677c442
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-09-14 20:58:34 -0300

    procedure para chamar selects

--- banco/p0_cons_chart.php
+++ banco/p0_cons_chart.php
@@ -1,12 +1,12 @@
 <?php
-/*buscar dados para grafico principal como JSON*/ 
+/*buscar dados para grafico principal como JSON*/
 include_once('../../conexao.php');
-$select_chart = ('SELECT pope.data_ref, (select periodo from lista_periodos where id = pope.periodo) as periodo, sum(lanc.liquido) as total from periodos_operacao pope JOIN operacao ope on pope.id = ope.periodo JOIN lancamentos lanc on lanc.operacao = ope.id GROUP by data_ref, pope.periodo;
-');
+$select_chart = "call grafico_inicial()";
 $prep = $con->prepare($select_chart);
 $prep->execute();
-while ($resultados = $prep->fetch(PDO::FETCH_ASSOC)){
-	$resulado[] = $resultados;
+while ($resultados = $prep->fetch(PDO::FETCH_ASSOC)) {
+    $resulado[] = $resultados;
 }
 echo json_encode($resulado);
+$prep->closeCursor()
 ?>
--- banco/p3_cons_cnpj_banco.php
+++ banco/p3_cons_cnpj_banco.php
@@ -1,28 +1,30 @@
 <?php
 include_once('../../conexao.php');
-function retorna($cnpj,$con){
-	$query_em = ("SELECT emp.id as id, emp.xCNPJ, emp.xNome, emp.fantasia, cep.cep, cep.estado,cep.cidade,cep.bairro,cep.rua FROM `empresas` emp left join `lista_cep` cep on cep.id = emp.cep where xCNPJ = ".$cnpj);
-	$prep = $con->prepare($query_em);
-	$prep->execute();
-	$contar = $prep->rowCount();
-	$linha = $prep ->fetch();
-	if ($contar > 0) {
-		$retorno['id']        = $linha['id'];
-		$retorno['cnpj']      = $linha['xCNPJ'];
-		$retorno['razao']     = $linha['xNome'];
-		$retorno['fantasia']  = $linha['fantasia'];
-		$retorno['cep']       = $linha['cep'];
-		$retorno['estado']    = $linha['estado'];
-		$retorno['cidade']    = $linha['cidade'];
-		$retorno['bairro']    = $linha['bairro'];
-		$retorno['rua']       = $linha['rua'];
-		$retorno['situacao']  = true;
-	}else{
-		$retorno['situacao'] = false;
-	}
-		return json_encode($retorno);
+function retorna($cnpj, $con)
+{
+    $query_em = ("call buscar_empresa('".$cnpj."')");
+    $prep = $con->prepare($query_em);
+    $prep->execute();
+    $contar = $prep->rowCount();
+    $linha = $prep ->fetch();
+    if ($contar > 0) {
+        $retorno['id']        = $linha['id'];
+        $retorno['cnpj']      = $linha['xCNPJ'];
+        $retorno['razao']     = $linha['xNome'];
+        $retorno['fantasia']  = $linha['fantasia'];
+        $retorno['cep']       = $linha['cep'];
+        $retorno['estado']    = $linha['estado'];
+        $retorno['cidade']    = $linha['cidade'];
+        $retorno['bairro']    = $linha['bairro'];
+        $retorno['rua']       = $linha['rua'];
+        $retorno['situacao']  = true;
+    } else {
+        $retorno['situacao'] = false;
+    }
+    $prep->closeCursor();
+    return json_encode($retorno);
 }
-if(isset($_GET['cnpj'])){
- echo retorna($_GET['cnpj'],$con);
+if (isset($_GET['cnpj'])) {
+    echo retorna($_GET['cnpj'], $con);
 }
 ?>
