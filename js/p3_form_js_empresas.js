d6df31753511b5580494c9b4f9ac50e21023ffbf
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-09-14 12:30:15 -0300

    melhoria de codigo

--- js/p3_form_js_empresas.js
+++ js/p3_form_js_empresas.js
@@ -1,115 +1,102 @@
-function cnpj_buscar(doc){
+function cnpj_buscar(doc) {
   api = new Object();
   cnpj = new Object();
-          cnpj.limpo      = retira_formatacao(doc); //tira a formatação
-          if (cnpj.limpo.length == 14){
-            retorno = valida_cnpj(cnpj.limpo); // valida o cnpj
-            if (retorno.situacao == true){
-              /* se situação positiva, consulta se ja existe*/
-              banco = busca_cnpj_banco(cnpj.limpo);
-              //api = busca_cnpj_api(cnpj.limpo); //busca na api
-            }else{
-            }
-            /* retornos ao formulario*/
-            document.getElementById('cnpj_cad').value = retorno.formatacao;
-          }else{
-          }
-        }
-        function busca_cnpj_banco(cnpj){
-         var $razao = $("input[name='razao']");
-         var $fantasia = $("input[name='fantasia']");
-         var $uf                = $("input[name='uf']");
-         var $cidade            = $("input[name='cidade']");
-         var $cep               = $("input[name='cep']");
-         var $bairro            = $("input[name='bairro']");
-         var $logradouro        = $("input[name='logradouro']");
-         var ret_banco;
-         $.getJSON('banco/p3_cons_cnpj_banco.php',{
-          cnpj: cnpj
-        },function(json){
-          ret_banco = json.situacao;
-          console.log(ret_banco);
-          if (ret_banco == true) {
-            $razao = $razao.val(json.razao);
-            $fantasia = $fantasia.val(json.fantasia);
-            $uf          = $uf.val(json.estado);
-            $cidade      = $cidade.val(json.cidade);
-            $cep         = $cep.val(json.cep);
-            $bairro      = $bairro.val(json.bairro);
-            $logradouro  = $logradouro.val(json.rua);
-            document.getElementById('submit').disabled = true;
-          }else{
-            document.getElementById('submit').disabled = false;
-          api = busca_cnpj_api(cnpj); //busca na api
-        }
-      });
-       }
-       function busca_cnpj_api(cnpj){
-        var retorno_cnpj = new Object();
-        ajax = new Object();
-        var url   = "https://brasilapi.com.br/api/cnpj/v1/"+cnpj;
-        $.ajax({
-          url:url,
-          type:"get",
-          dataType: "json",
-          success:function(dados_cnpj){
-            auto_send(dados_cnpj);
-            document.getElementById('fantasia').readOnly = true;
-            document.getElementById('razao').value      = dados_cnpj.razao_social;
-            document.getElementById('fantasia').value   = dados_cnpj.nome_fantasia;
-            document.getElementById('uf').value         = dados_cnpj.uf;
-            document.getElementById('cidade').value     = dados_cnpj.municipio;
-            document.getElementById('cep').value        = dados_cnpj.cep;
-            document.getElementById('bairro').value     = dados_cnpj.bairro;
-            rua = dados_cnpj.descricao_tipo_logradouro;
-            rua += " ";
-            rua += dados_cnpj.logradouro;
-            document.getElementById('logradouro').value = rua;
-            if (dados_cnpj.nome_fantasia=='') {
-              document.getElementById('fantasia').readOnly = false;
-            }else{
-              document.getElementById('fantasia').readOnly = true;
-            }
-          }
-        })
+  cnpj.limpo = retira_formatacao(doc); //tira a formatação
+  if (cnpj.limpo.length == 14) {
+    retorno = valida_cnpj(cnpj.limpo); // valida o cnpj
+    if (retorno.situacao == true) {
+      /* se situação positiva, consulta se ja existe*/
+      banco = busca_cnpj_banco(cnpj.limpo);
+      //api = busca_cnpj_api(cnpj.limpo); //busca na api
+    } else {
+    }
+    /* retornos ao formulario*/
+    document.getElementById("cnpj_cad").value = retorno.formatacao;
+  } else {
+  }
+}
+function busca_cnpj_banco(cnpj) {
+  var $razao = $("input[name='razao']");
+  var $fantasia = $("input[name='fantasia']");
+  var $uf = $("input[name='uf']");
+  var $cidade = $("input[name='cidade']");
+  var $cep = $("input[name='cep']");
+  var $bairro = $("input[name='bairro']");
+  var $logradouro = $("input[name='logradouro']");
+  var ret_banco;
+  $.getJSON(
+    "banco/p3_cons_cnpj_banco.php",
+    {
+      cnpj: cnpj,
+    },
+    function (json) {
+      ret_banco = json.situacao;
+      console.log(ret_banco);
+      if (ret_banco == true) {
+        $razao = $razao.val(json.razao);
+        $fantasia = $fantasia.val(json.fantasia);
+        $uf = $uf.val(json.estado);
+        $cidade = $cidade.val(json.cidade);
+        $cep = $cep.val(json.cep);
+        $bairro = $bairro.val(json.bairro);
+        $logradouro = $logradouro.val(json.rua);
+        document.getElementById("submit").disabled = true;
+      } else {
+        document.getElementById("submit").disabled = false;
+        api = busca_cnpj_api(cnpj); //busca na api
       }
-      /*envio de dados para Edição de cadastro*/
-      $("#form_cad_emp").submit(function(e){
-        e.preventDefault();
-        $form = $(this);
-        var formdata = new FormData($form[0]);
-          //cria conexao
-          var request = new XMLHttpRequest();
-          //conexao arquivo
-          request.open('post','banco/p3_form_empresas_editar.php');
-          request.send(formdata);
-          document.getElementById('form_cad_emp').reset();
-          document.getElementById('fecha_modal').click();
-          document.location.reload(true);
-        })
+    }
+  );
+}
+function busca_cnpj_api(cnpj) {
+  var retorno_cnpj = new Object();
+  ajax = new Object();
+  var url = "https://brasilapi.com.br/api/cnpj/v1/" + cnpj;
+  $.ajax({
+    url: url,
+    type: "get",
+    dataType: "json",
+    success: function (dados_cnpj) {
+      auto_send(dados_cnpj);
+    },
+  });
+}
+/*envio de dados para Edição de cadastro*/
+$("#form_cad_emp").submit(function (e) {
+  e.preventDefault();
+  $form = $(this);
+  var formdata = new FormData($form[0]);
+  //cria conexao
+  var request = new XMLHttpRequest();
+  //conexao arquivo
+  request.open("post", "banco/p3_form_empresas_editar.php");
+  request.send(formdata);
+  document.getElementById("form_cad_emp").reset();
+  document.getElementById("fecha_modal").click();
+  document.location.reload(true);
+});
 
-        /* cadastrar automaticamente */
+/* cadastrar automaticamente */
 
-        function auto_send(empresa) {
-          envio = new Object();
-          envio.cnpj_cad = empresa.cnpj
-          envio.razao = empresa.razao_social
-          envio.fantasia = empresa.nome_fantasia
-          envio.uf = empresa.uf
-          envio.cidade = empresa.municipio
-          envio.cep = empresa.cep
-          envio.bairro = empresa.bairro
-          envio.desclog = empresa.descricao_tipo_logradouro
-          envio.logradouro = empresa.logradouro
+function auto_send(empresa) {
+  envio = new Object();
+  envio.cnpj_cad = empresa.cnpj;
+  envio.razao = empresa.razao_social;
+  envio.fantasia = empresa.nome_fantasia;
+  envio.uf = empresa.uf;
+  envio.cidade = empresa.municipio;
+  envio.cep = empresa.cep;
+  envio.bairro = empresa.bairro;
+  envio.desclog = empresa.descricao_tipo_logradouro;
+  envio.logradouro = empresa.logradouro;
 
-           $.ajax({
-             url:'banco/p3_form_empresas_cad.php',
-             type:'POST',
-             data:{data:envio},
-             success: function(result){
-                document.getElementById('Retorno_cadastro').innerHTML = 'Cadastrada';
-           }
-           });
-        
-        }
-        
+  $.ajax({
+    url: "banco/p3_form_empresas_cad.php",
+    type: "POST",
+    data: { data: envio },
+    success: function () {
+      document.getElementById("Retorno_cadastro").innerHTML = "Cadastrada";
+      busca_cnpj_banco(envio.cnpj_cad);
+    },
+  });
+}
--- main/p3_main_empresas.php
+++ main/p3_main_empresas.php
@@ -1,103 +1,108 @@
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Empresas cadastradas</h1>
-    <div class="btn-toolbar mb-2 mb-md-0">
-      <div class="btn-group me-2">
-        <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#cadastro_empresa" accesskey="n">Novo</button>
-      </div>
+    <div
+        class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Empresas cadastradas</h1>
+        <div class="btn-toolbar mb-2 mb-md-0">
+            <div class="btn-group me-2">
+                <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal"
+                    data-bs-target="#cadastro_empresa" accesskey="n">Novo</button>
+            </div>
+        </div>
     </div>
-  </div>
-  <?php 
+    <?php 
   if (isset($_SESSION['mensagem'])) {
     echo $_SESSION['mensagem'];
     unset($_SESSION['mensagem']);
   }
   ?>
-  <h2></h2>
-  <div class="table-responsive">
-    <table class="table table-hover table-striped table-sm">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Razão social</th>
-          <th scope="col">Nome fantasia</th>
-          <th scope="col">Cnpj</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php 
+    <h2></h2>
+    <div class="table-responsive">
+        <table class="table table-hover table-striped table-sm">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Razão social</th>
+                    <th scope="col">Nome fantasia</th>
+                    <th scope="col">Cnpj</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
         $queryu = "SELECT * from empresas order by xNome";
         $resultado = $con ->prepare($queryu);
         $resultado ->execute();
         $cnt = 1;
         while ($linha=$resultado->fetch()) {
           ?>
-          <tr>
-            <td>Abrir</td>
-            <td><?php echo $linha['xNome']; ?></td>
-            <td><?php echo $linha['fantasia']; ?></td>
-            <td><?php echo $linha['xCNPJ']; ?></td>
-            
-          </tr>
-          <?php
+                <tr>
+                    <td>Abrir</td>
+                    <td><?php echo $linha['xNome']; ?></td>
+                    <td><?php echo $linha['fantasia']; ?></td>
+                    <td><?php echo $linha['xCNPJ']; ?></td>
+
+                </tr>
+                <?php
         }
         ?>
-        
-      </tbody>
-    </table>
-  </div>
-  
-  <!-- Modal -->
-  <div class="modal fade" id="cadastro_empresa" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Cadastro de empresa </h5>
-          <button type="button" id="fecha_modal" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <form method="post" id="form_cad_emp" action="a.php">
-            <div class="mb-3">
-              <label for="cnpj_cad" class="form-label">CNPJ</label>
-              <input type="text" oninput="cnpj_buscar(value);" class="form-control" id="cnpj_cad" name="cnpj_cad" maxlength="18" autocomplete="off">
-              <div class="small mt-2" id="or_val"></div>
-            </div>
-            <div class="mb-3">
-              <label for="razao" class="form-label">Razão Social</label>
-              <input type="text" class="form-control" id="razao" name="razao" readonly>
-            </div>
-            <div class="mb-3">
-              <label for="fantasia" class="form-label">Nome Fantazia</label>
-              <input type="text" class="form-control" id="fantasia" name="fantasia" readonly required>
-            </div>
-            <div class="mb-3">
-              <label for="cep" class="form-label">Cep</label>
-              <input type="text" class="form-control" id="cep" name="cep" readonly>
-            </div>
-            <div class="mb-3">
-              <label for="uf" class="form-label">UF</label>
-              <input type="text" class="form-control" id="uf" name="uf" readonly>
-            </div>
-            <div class="mb-3">
-              <label for="cidade" class="form-label">Cidade</label>
-              <input type="text" class="form-control" id="cidade" name="cidade" readonly>
-            </div>
-            <div class="mb-3">
-              <label for="bairro" class="form-label">Bairro</label>
-              <input type="text" class="form-control" id="bairro" name="bairro" readonly>
-            </div>
-            <div class="mb-3">
-              <label for="logradouro" class="form-label">Logradrouro</label>
-              <input type="text" class="form-control" id="logradouro" name="logradouro" readonly>
-            </div>
-          </form>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-          <button type="submit" form="form_cad_emp" id="submit" class="btn btn-primary">Gravar</button>
-        </div>
-      </div>
+
+            </tbody>
+        </table>
     </div>
-  </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="cadastro_empresa" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Cadastro de empresa <div id="Retorno_cadastro"></div>
+                    </h5>
+                    <button type="button" id="fecha_modal" class="btn-close" data-bs-dismiss="modal"
+                        aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form method="post" id="form_cad_emp" action="a.php">
+                        <div class="mb-3">
+                            <label for="cnpj_cad" class="form-label">CNPJ</label>
+                            <input type="text" oninput="cnpj_buscar(value);" class="form-control" id="cnpj_cad"
+                                name="cnpj_cad" maxlength="18" autocomplete="off">
+                            <div class="small mt-2" id="or_val"></div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="razao" class="form-label">Razão Social</label>
+                            <input type="text" class="form-control" id="razao" name="razao" readonly>
+                        </div>
+                        <div class="mb-3">
+                            <label for="fantasia" class="form-label">Nome Fantazia</label>
+                            <input type="text" class="form-control" id="fantasia" name="fantasia" readonly required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="cep" class="form-label">Cep</label>
+                            <input type="text" class="form-control" id="cep" name="cep" readonly>
+                        </div>
+                        <div class="mb-3">
+                            <label for="uf" class="form-label">UF</label>
+                            <input type="text" class="form-control" id="uf" name="uf" readonly>
+                        </div>
+                        <div class="mb-3">
+                            <label for="cidade" class="form-label">Cidade</label>
+                            <input type="text" class="form-control" id="cidade" name="cidade" readonly>
+                        </div>
+                        <div class="mb-3">
+                            <label for="bairro" class="form-label">Bairro</label>
+                            <input type="text" class="form-control" id="bairro" name="bairro" readonly>
+                        </div>
+                        <div class="mb-3">
+                            <label for="logradouro" class="form-label">Logradrouro</label>
+                            <input type="text" class="form-control" id="logradouro" name="logradouro" readonly>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                    <button type="submit" form="form_cad_emp" id="submit" class="btn btn-primary">Gravar</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
 <script src="js/p3_form_js_empresas.js"></script>
